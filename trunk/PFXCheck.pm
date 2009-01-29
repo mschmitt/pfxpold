3663057775cf1dcd551e75573aedbf9a212d6c57
martin <martin@484cc3a2-7d94-4784-8140-313c9a92d1de>
2009-01-29 21:11:25 +0000

    Inconsistency in check module
    git-svn-id: file:///opt/svn/scripts/postfix/pfxpold@429 484cc3a2-7d94-4784-8140-313c9a92d1de

--- trunk/PFXCheck.pm
+++ trunk/PFXCheck.pm
@@ -33,7 +33,7 @@ sub do_check($){
 	# Randomly accept the request
 	if (rand(0) > 0.5){
 		return "OK (you win!)";
-		&main::dolog("debug", "PFX-PERMIT: client_name=$client_name client_address=$client_address sender=$sender");
+		&main::dolog("info", "PFX-PERMIT: client_name=$client_name client_address=$client_address sender=$sender");
 	}
 
 	&main::dolog("info", "PFX-REJECT: client_name=$client_name client_address=$client_address sender=$sender");
